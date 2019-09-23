62139af7a7d382e98af8f8e74402fd42e34a9409
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-09-23 10:07:53 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+    def initialize
+        super
+        @knowledge = []
+    end
+
+    def learn(info)
+        @knowledge.push(info)
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
