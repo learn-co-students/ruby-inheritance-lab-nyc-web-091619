52bd58613a4cbc57484696777c3784c9c09405e8
michelle-m-lee <mlee092@gmail.com>
2019-09-25 16:38:05 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize #initializes knowledge with empty array
+        @knowledge = []
+    end
+
+    def learn(knowledge) #takes a string of knowledge and adds to the knowledge array - shovel
+        @knowledge << knowledge
+    end
+
+    def knowledge #returns knowledge array
+        @knowledge
+    end
+
+
 
 end
